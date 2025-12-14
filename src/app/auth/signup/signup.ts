97263f1f3faaaa2f15128c06b370d9d5b4eba30b
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-14 19:25:21 +0530

    signup implementation

--- src/app/auth/signup/signup.ts
+++ src/app/auth/signup/signup.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
+  username:string='';
+  email:string='';
+  password:string='';
+  role:string='user';
+
+  onSignup(){
+    console.log('Username:', this.username);
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Role:', this.role);
+  }
 
 }
