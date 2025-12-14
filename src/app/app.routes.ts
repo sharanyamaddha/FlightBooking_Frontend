b34b1771fbfdfb620084c48ad983c5497d9206d8
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-14 11:46:11 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Signup } from './auth/signup/signup';
+import { Login } from './auth/login/login'; 
+import { FlightSearch } from './flight/flight-search/flight-search';    
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'register',component:Signup},
+    {path:'login',component:Login},
+    {path:'search',component:FlightSearch}
+];
