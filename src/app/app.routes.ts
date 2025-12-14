39b348b563accc5af3b652d8e06c41dc2de450e0
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-14 11:49:55 +0530

    creeated home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Signup } from './auth/signup/signup';
 import { Login } from './auth/login/login'; 
-import { FlightSearch } from './flight/flight-search/flight-search';    
+import { FlightSearch } from './flight/flight-search/flight-search';  
+import { Home } from './home/home';  
 
 export const routes: Routes = [
+    {path:'',component:Home},
     {path:'register',component:Signup},
     {path:'login',component:Login},
     {path:'search',component:FlightSearch}
