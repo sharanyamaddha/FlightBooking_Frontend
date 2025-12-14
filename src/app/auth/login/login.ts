c1a8c6768523e23c3b69faa9356bb76f9a1d7940
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-14 19:26:01 +0530

    signin implementation

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username:string='';
+  password:string='';
 
+  onSignin(){
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+
+  }
 }
