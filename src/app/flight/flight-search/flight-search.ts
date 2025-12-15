04e9192164a8382f86380a3b2888b693b25ec7c7
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-15 17:01:44 +0530

    added search results component

--- src/app/flight/flight-search/flight-search.ts
+++ src/app/flight/flight-search/flight-search.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Flight } from '../../services/flight';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flight-search',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './flight-search.html',
   styleUrl: './flight-search.css',
 })
-export class FlightSearch {
+export class FlightSearch implements OnInit {
 
+  flights: any[] = [];
+
+  constructor(private flightService: Flight) {}
+
+  ngOnInit() {
+    this.flights = this.flightService.getResults();
+  }
 }
