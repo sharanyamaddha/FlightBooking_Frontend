4076adc624ad6ff9f19c904b54036c8f637d87d4
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-24 14:22:03 +0530

    modified toast component

--- src/app/components/toast/toast.ts
+++ src/app/components/toast/toast.ts
@@ -1,28 +1,72 @@
 import { CommonModule } from '@angular/common';
-import { Component,Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './toast.html',
-  template:
-  `<div class="toast-msg" *ngIf="show">
-      {{ message }}
-    </div>`,
-  styleUrl: './toast.css',
+  styleUrl: './toast.css'
 })
 export class Toast {
 
-   @Input() message = '';
+  message = '';
   show = false;
+  isConfirm = false;
+private onConfirm?: () => void;
 
-  showToast(msg: string) {
-    this.message = msg;
-    this.show = true;
 
-    setTimeout(() => {
-      this.show = false;
-    }, 2500);
+  private afterClose: (() => void) | null = null;
+
+showToast(msg: string, cb?: () => void) {
+    this.isConfirm = false;       
+  this.message = msg;
+  this.afterClose = cb || null;
+  this.show = true;
+}
+
+showConfirm(msg: string, action: () => void) {
+  this.isConfirm = true;
+  this.message = msg;
+  this.onConfirm = action;
+  this.show = true;
+}
+
+showAuto(msg: string, delay: number, callback?: () => void) {
+  this.isConfirm = false;
+  this.message = msg;
+  this.show = true;
+
+  setTimeout(() => {
+    this.close();
+    if (callback) callback();
+  }, delay);
+}
+
+showAndClose(msg: string, delay: number, after?: () => void) {
+  this.isConfirm = false; 
+  this.message = msg;
+  this.show = true;
+
+  setTimeout(() => {
+    this.show = false;
+    if (after) after();
+  }, delay);
+}
+
+
+
+confirm() {
+  if (this.onConfirm) this.onConfirm();
+  this.close();
+}
+
+close() {
+  this.show = false;
+  if (this.afterClose) {
+    this.afterClose();
+    this.afterClose = null;
   }
+}
+
 }
