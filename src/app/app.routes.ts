2f8f0f217394929a53ef00350c52eba30db7c726
Sharanya Maddha <sharanyamaddha@gmail.com>
2025-12-25 11:12:37 +0530

    added guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,17 @@ import { BookFlight } from './components/book-flight/book-flight';
 import { BookingsHistory } from './components/bookings-history/bookings-history';
 import { AddFlights } from './components/add-flights/add-flights';
 import { ChangePassword } from './components/change-password/change-password';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path:'',component:Home},
     {path:'register',component: Signup},
     {path:'login',component:Login},
     {path:'flight-search',component:FlightSearch},
-    { path: 'profile', component: Profile },
-    {path:'book-flight/:flightId',component:BookFlight},
-    {path:'bookings-history',component:BookingsHistory},
-    {path:'add-flights',component:AddFlights},
-    {path:'change-password',component:ChangePassword}
+    {path: 'profile', component: Profile },
+    {path:'book-flight/:flightId',component:BookFlight,canActivate:[authGuard]},
+    {path:'bookings-history',component:BookingsHistory,canActivate:[authGuard]},
+    {path:'add-flights',component:AddFlights,canActivate:[authGuard]},
+    {path:'change-password',component:ChangePassword,canActivate:[authGuard]}
 
 ];
